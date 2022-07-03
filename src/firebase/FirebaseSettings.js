b6a6aeddb5bcfcc1c87bc8de86be88b890476090
ranjithk22 <32597131+ranjithk22@users.noreply.github.com>
2022-07-03 19:16:41 +0530

    Delete FirebaseSettings.js

--- src/firebase/FirebaseSettings.js
+++
@@ -1,9 +0,0 @@
-export const FirebaseSetting = {
-   apiKey: "AIzaSyBsB_92voGc5DaEY2g__c0bV6JADPLKjjg",
-    authDomain: "industrial-fx-287011.firebaseapp.com",
-    projectId: "industrial-fx-287011",
-    storageBucket: "industrial-fx-287011.appspot.com",
-    messagingSenderId: "21438605014",
-    appId: "1:21438605014:web:be412d319e715ecc120b77",
-    measurementId: "G-TT1562SHKC"
-}
